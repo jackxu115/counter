5e43f667800d976e835e2d52fc3cc33730baaf3f
pengxu <pengxu19890115@gmail.com>
2019-11-12 13:16:31 -0800

    add reset button in the counter file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 import { increment, decrement } from "../actions";
 import { connect } from "react-redux";
 
-class Counter extends Component {
-  render() {
-    const { value, increment, decrement } = this.props;
-    return (
-      <div>
-        <h1>Count: {value}</h1>
-        <button onClick={increment}>+</button>
-        <button onClick={decrement}>-</button>
-      </div>
-    );
-  }
-}
+const Counter = ({ value, increment, decrement }) => (
+  <div>
+    <h1>Count: {value}</h1>
+    <button onClick={increment}>+</button>
+    <button onClick={decrement}>-</button>
+    <button>Reset</button>
+  </div>
+);
 
 const mapStateToProps = state => {
   return {
