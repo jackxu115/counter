c25eb35a9997acb863cfe2eab2f4435cb9e0af42
pengxu <pengxu19890115@gmail.com>
2019-11-12 13:11:29 -0800

    edit store, reducers and counter files

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,10 +1,35 @@
 import React, { Component } from "react";
+import { increment, decrement } from "../actions";
+import { connect } from "react-redux";
 
 class Counter extends Component {
   render() {
     const { value, increment, decrement } = this.props;
-    return <h1>Clicked: {value}</h1>;
+    return (
+      <div>
+        <h1>Count: {value}</h1>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      </div>
+    );
   }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+  return {
+    value: state.count
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => {
+      dispatch(increment());
+    },
+    decrement: () => {
+      dispatch(decrement());
+    }
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
-export default (state = 0, action) => {
+const initialState = { count: 0 };
+
+export default (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
-      return Object.assign({}, state, { state: state + 1 });
+      return Object.assign({}, state, { count: state.count + 1 });
     case "DECREMENT":
-      return Object.assign({}, state, { state: state - 1 });
+      return Object.assign({}, state, { count: state.count - 1 });
     default:
       return state;
   }
